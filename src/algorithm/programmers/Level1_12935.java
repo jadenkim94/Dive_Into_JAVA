0bc2c39bb9aafc01f4f53a6ffdf541f7af40302c
jaden <jonghaha7@gmail.com>
2021-01-01 20:06:45 +0900

    프로그래머스 https://programmers.co.kr/learn/courses/30/lessons/12935

---
+++ src/algorithm/programmers/Level1_12935.java
@@ -0,0 +1,63 @@
+package algorithm.programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+https://programmers.co.kr/learn/courses/30/lessons/12935
+문제 설명
+정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution 을 완성해주세요.
+ 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 예를들어 arr 이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+
+제한 조건
+arr 은 길이 1 이상인 배열입니다.
+인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
+ */
+public class Level1_12935 {
+    public static void main(String[] args) {
+        int[] arr = {10};
+        System.out.println(solution(arr)[0]);
+
+        int[] brr = {4, 3, 2, 1};
+        Arrays.stream(solution(brr)).forEach(System.out::println);
+
+    }
+
+    public static int[] solution(int[] arr) {
+        int[] answer = {};
+
+        if (arr.length <= 1) {
+            answer = new int[1];
+            answer[0] = -1;
+            return answer;
+        }
+
+        int min = Integer.MAX_VALUE;
+        int index = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= min) {
+                min = arr[i];
+                index = i;
+            }
+        }
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (Integer e : arr) {
+            arrayList.add(e);
+        }
+
+        arrayList.remove(index);
+
+
+        Integer[] intArr = arrayList.toArray(Integer[]::new);
+
+        answer = Arrays.stream(intArr).mapToInt(Integer::intValue).toArray();
+
+        return answer;
+
+
+    }
+
+}
+
