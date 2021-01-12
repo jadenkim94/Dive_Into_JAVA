42669004c54ef4dab6ba8a43d1718a4ea5533924
jaden <jonghaha7@gmail.com>
2021-01-12 17:22:14 +0900

    :white_check_mark: 프로그래머스 https://programmers.co.kr/learn/courses/30/lessons/12926

--- src/algorithm/programmers/Level1_12926.java
+++ src/algorithm/programmers/Level1_12926.java
@@ -3,7 +3,8 @@ package algorithm.programmers;
 https://programmers.co.kr/learn/courses/30/lessons/12926
 시저 암호
 문제 설명
-어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다. 예를 들어 AB는 1만큼 밀면 BC가 되고, 3만큼 밀면 DE가 됩니다.
+어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다.
+예를 들어 AB는 1만큼 밀면 BC가 되고, 3만큼 밀면 DE가 됩니다.
 z는 1만큼 밀면 a가 됩니다. 문자열 s와 거리 n을 입력받아 s를 n만큼 민 암호문을 만드는 함수, solution을 완성해 보세요.
 Z 를 1만큼 밀면 A
 
@@ -15,46 +16,38 @@ n은 1 이상, 25이하인 자연수입니다.
  */
 public class Level1_12926 {
     public static void main(String[] args) {
-        String s = "zZ yY";
-        System.out.println(solution(s, 20));
+        String s = " a b ";
+        System.out.println(solution(s, 1));
+
 
     }
 
+    // 65 ~ 90 : A ~ Z
+    // 97 ~ 122 : a ~ z
+    // 32 : " "
+
     public static String solution(String s, int n) {
         String answer = "";
-        char[] chars = s.toCharArray();
-        StringBuilder stringBuilder = new StringBuilder();
-        for(int i = 0 ; i < chars.length; i++){
-            if(!(chars[i]==' ')){
-                if(chars[i]>='a' && chars[i]<='z'){
-                    int check = chars[i]+n;
-                    if (check > 'z'){
-                        check = 'a'-1;
-                        check = check + n;
-                        char valChar = (char)check;
-                        stringBuilder.append(valChar);
-                    } else {
-                        char valChar = (char) check;
-                        stringBuilder.append(valChar);
-                    }
-                } else if(chars[i] >= 'A' && chars[i] <= 'Z'){
-                    int check = chars[i]+n;
-                    if(check > 'Z'){
-                        check = 'A'-1;
-                        check = check + n;
-                        char valChar = (char)check;
-                        stringBuilder.append(valChar);
-                    } else{
-                        char valChar = (char)check;
-                        stringBuilder.append(valChar);
-                    }
+        char[] elements = s.toCharArray();
+
+        for(int i = 0 ; i < elements.length; i++){ // i = 0,1,2
+            if(elements[i]>=65 && elements[i]<=90){
+                int change = elements[i] + n;
+                if( change > 90 ){
+                    change = change - 26;
                 }
-            } else {
-                stringBuilder.append(" ");
+                answer += Character.toString(change);
+            } else if ( elements[i] >= 97 && elements[i] <= 122){
+                int change = elements[i] + n;
+                if (change > 122)
+                    change = change - 26;
+                answer += Character.toString(change);
+            } else if ( elements[i] == 32){
+                answer += elements[i];
             }
         }
 
-        answer = stringBuilder.toString();
+
         return answer;
     }
 }
